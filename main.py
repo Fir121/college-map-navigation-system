e34fa49bac337ed9c1c0f7417b5ba4bc86c427fc
Mohamed Firas <62911996+Fir121@users.noreply.github.com>
2024-04-22 13:19:34 +0400

    bugfix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ m = MapBuilder()
 
 my_position = [55.41992602739069,25.131639929188452]
 pf = PathFinder()
-pf.giveBestPath(m.map_BPDC, my_position, m.p, m.b, "Auditorium")
+pf.giveBestPath(m.map_BPDC, [my_position], m.p, m.b, "Auditorium")
 
 # map_BPDC.save("map.html")
 m.map_BPDC.show_in_browser()
