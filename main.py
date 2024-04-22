531984b479637d8f484341b9ac2a0632b0a65d56
Mohamed Firas <62911996+Fir121@users.noreply.github.com>
2024-04-22 18:52:51 +0400

    update reqs

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from classes.pathfinder import PathFinder
 m = MapBuilder()
 
 my_position = [55.41992602739069,25.131639929188452]
+
 pf = PathFinder()
-pf.giveBestPath(m.map_BPDC, [my_position], m.p, m.b, "Auditorium")
+m.p.markerPath(m.map_BPDC)
+# pf.giveBestPath(m.map_BPDC, [my_position], m.p, m.b, "Auditorium")
+
+
 
 # map_BPDC.save("map.html")
 m.map_BPDC.show_in_browser()
